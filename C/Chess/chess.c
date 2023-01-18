aa3584fe74b439b82e32460dd3ee80628a953d35
CheekyHarlequin <bastianklingspor@gmail.com>
2023-01-18 13:31:46 +0100

    Cleanup and fixes

--- C/Chess/chess.c
+++ C/Chess/chess.c
@@ -1,118 +1,98 @@
-#include <stdio.h>
-#include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <stdio.h>
+#include <string.h>
 
-#define PIECES 100;
-#define BOARD 1000;
+//Sizes in pixels
+#define PIECE_SIZE 100;
+#define BOARD_SIZE 1000;
 
-//global things like renderer
-Uint32 render_flags;
-SDL_Renderer* rend;
+SDL_Renderer* renderer;
 SDL_Surface* surface;
+SDL_Window* window;
+
+char pieceFilePaths[32][3] = { "bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP", "bP", "bP",
+																"bP", "bP", "bP", "bP", "bP", "wP", "wP", "wP", "wP", "wP", "wP",
+																"wP", "wP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR" };
+
+struct SDL_Rect piecerects[32];
 
 
-char srcofpieces[32][24]=
-                      {
-                      "textures/bR.svg", "textures/bN.svg", "textures/bB.svg", "textures/bQ.svg", "textures/bK.svg", "textures/bB.svg", "textures/bN.svg", "textures/bR.svg",
-                      "textures/bP.svg", "textures/bP.svg", "textures/bP.svg", "textures/bP.svg", "textures/bP.svg", "textures/bP.svg", "textures/bP.svg", "textures/bP.svg",
-                      "textures/wP.svg", "textures/wP.svg", "textures/wP.svg", "textures/wP.svg", "textures/wP.svg", "textures/wP.svg", "textures/wP.svg", "textures/wP.svg",
-                      "textures/wR.svg", "textures/wN.svg", "textures/wB.svg", "textures/wQ.svg", "textures/wK.svg", "textures/wB.svg", "textures/wN.svg", "textures/wR.svg"
-                        };
 
-
-/*
-  backupPieces[32][2]= {
-                      "bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",
-                      "bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP",
-                      "wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP",
-                      "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"
-                        };
-*/
 void play();
 void normalBoard();
 
-int main()
-{
-  if(SDL_Init(SDL_INIT_VIDEO))
-  {
-    printf("error initializing SDL: %s\n", SDL_GetError());
-    return 0;
-  }
-  SDL_Window* win =SDL_CreateWindow("Test",
-                                    SDL_WINDOWPOS_CENTERED,
-                                    SDL_WINDOWPOS_CENTERED,
-                                    1000, 1000, 0);
-                                    render_flags = SDL_RENDERER_ACCELERATED;
-  render_flags = SDL_RENDERER_ACCELERATED;
-  rend = SDL_CreateRenderer(win, -1, render_flags);
+int main() {
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("Error: %s\n", SDL_GetError());
+		return 0;
+	}
 
+	window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
 
-    play();
-    while(1);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	play();
 
-  return 0;
+	SDL_Delay(3000);
+
+	return 0;
 }
 
-void play()
-  {
-    normalBoard();
+void play() {
+	normalBoard();
+}
 
+void normalBoard() {
+	//Draw the board
+	SDL_Rect boardrect;
+	surface = IMG_Load("textures/Chess.jpg");
+	SDL_Texture* boardTex = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_QueryTexture(boardTex, NULL, NULL, &boardrect.w, &boardrect.h);
 
- }
+	//resize and change pos of the board rect.
+	boardrect.w = BOARD_SIZE;
+	boardrect.h = BOARD_SIZE;
+	boardrect.x = 0;
+	boardrect.y = 0;
+	SDL_FreeSurface(surface);
+	SDL_RenderCopy(renderer, boardTex, NULL, &boardrect);
 
-void normalBoard()
-{
-  // making the Board
-  SDL_Rect boardrect;
-  surface = IMG_Load("textures/Chess.jpg");
-  SDL_Texture* boardTex = SDL_CreateTextureFromSurface(rend, surface);
-  SDL_QueryTexture(boardTex, NULL, NULL, &boardrect.w, &boardrect.h);
+	SDL_Texture* pieceTex;
+	char counter = 1;
 
-  //resize and change pos of the board rect.
-  boardrect.w = BOARD;
-  boardrect.h = BOARD;
-  boardrect.x = 0;
-  boardrect.y = 0;
-  SDL_FreeSurface(surface);
-  SDL_RenderCopy(rend, boardTex, NULL, &boardrect);
+	for (int i = 0; i < 32; i++) {
+		char path[32] = { "textures/" };
+		strcat(path, pieceFilePaths[i]);
+		strcat(path, ".png");
 
-  //puting pieces on it
+		surface = IMG_Load(path);
 
-  SDL_Rect piecerect;
-  SDL_Texture* pieceTex;
+		pieceTex = SDL_CreateTextureFromSurface(renderer, surface);
 
-  char counter = 1;
+		SDL_QueryTexture(pieceTex, NULL, NULL, &piecerects[i].w, &piecerects[i].h);
 
-  for(int i=0; i<32;i++)
-  {
-    surface = IMG_Load(srcofpieces[i]);
-    pieceTex = SDL_CreateTextureFromSurface(rend, surface);
-    SDL_QueryTexture(pieceTex, NULL, NULL, &piecerect.w, &piecerect.h);
-    piecerect.w = PIECES;
-    piecerect.h = PIECES;
-    if(i<15)
-    {
-      if(i<8)
-      {
-        piecerect.x = (100*counter);
-        piecerect.y = 100;
-        counter++;
-      }
-      else
-      {
-        counter = 1;
-        piecerect.x = (100*counter);
-        piecerect.y = 200;
-      }
+		piecerects[i].w = PIECE_SIZE;
+		piecerects[i].h = PIECE_SIZE;
 
-      SDL_FreeSurface(surface);
-      SDL_RenderCopy(rend, pieceTex, NULL, &piecerect);
-    }
-  }
+		if (i < 16) {
+			counter = ((counter >= 9) ? 1 : counter);
+			piecerects[i].x = (100 * counter);
+			piecerects[i].y = ((i < 8 && i != 15) ? 100 : 200);
+		} else {
+			counter = ((counter >= 9) ? 1 : counter);
+			piecerects[i].x = (100 * counter);
+			piecerects[i].y = ((i < 24) ? 700 : 800);
+		}
 
-  SDL_RenderPresent(rend);
-  SDL_DestroyTexture(boardTex);
+		SDL_FreeSurface(surface);
+		SDL_RenderCopy(renderer, pieceTex, NULL, &piecerects[i]);
+		SDL_DestroyTexture(pieceTex);
+		counter++;
+	}
+
+	SDL_RenderPresent(renderer);
+
+	SDL_DestroyTexture(boardTex);
 }
